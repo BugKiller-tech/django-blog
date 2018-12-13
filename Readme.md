0ee1c8b268ccf0aa1614f7c8242be5bd7c9cd4de
RoraxHan(kyl) <hkg328@outlook.com>
2018-12-13 11:27:42 -0500

    templates

---
+++ Readme.md
@@ -0,0 +1,19 @@
+## start the proejct
+```
+django-admin startproject django_project
+```
+> python manage.py runserver
+
+then server will be run.
+
+> python manage.py migraion
+
+> python manage.py startapp blog
+
+
+> make templates folder inside blog folder
+> and add this 'blog.apps.BlogConfig' into settins.py INSTALLED_APPS
+This is needed so that django find the templates and models.
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+posts = [
+  {
+    'author': 'Corey',
+    'title': 'blog post1',
+    'content': 'first blog post',
+    'date_created': 'aug 27, 2018'
+  },
+  {
+    'author': 'Corey 222',
+    'title': 'blog post1',
+    'content': 'first blog post',
+    'date_created': 'aug 27, 2018'
+  }
+]
 
 # Create your views here.
+
+def home(request):
+  # return HttpResponse('<h1>welcome to my blog</h1>')
+  context = {
+    'title': 'latest blogs',
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
+
+def about(request):
+  return render(request, 'blog/about.html')
+
+def error_404(request):
+  return HttpResponse('<h1>Page not found!</h1>', { 'title': 'about page' })
